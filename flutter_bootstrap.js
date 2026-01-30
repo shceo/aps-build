82792ef44fca235ad0bf5dab4f2b8754f93f1faa
╔ shceo ╝ 大 市 文 <104291367+shceo@users.noreply.github.com>
2026-01-30 12:16:36 +0500

    ы

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"c29809135135e262a912cf583b2c90deb9ded6
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2667442336"
+    serviceWorkerVersion: "2274068996"
   }
 });
--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -96,14 +96,14 @@ const RESOURCES = {"assets/AssetManifest.bin": "4b85bb333c9724d6df5df8321c155fcf
 "favicon.png": "467dd8d7629961184e38db90de60c21b",
 "firebase-messaging-sw.js": "70184c1314be09a95cc12ec2c74ac2b0",
 "flutter.js": "888483df48293866f9f41d3d9274a779",
-"flutter_bootstrap.js": "902cde223ac7b9dcdbbf00429ef512f8",
+"flutter_bootstrap.js": "5ec4aa7f97e6dcc6c8988e728a5b10cd",
 "icons/Icon-192.png": "467dd8d7629961184e38db90de60c21b",
 "icons/Icon-512.png": "467dd8d7629961184e38db90de60c21b",
 "icons/Icon-maskable-192.png": "467dd8d7629961184e38db90de60c21b",
 "icons/Icon-maskable-512.png": "467dd8d7629961184e38db90de60c21b",
 "index.html": "4931d24ec539c6ec3298133edcac5e95",
 "/": "4931d24ec539c6ec3298133edcac5e95",
-"main.dart.js": "50b6b57815e1c3fcab667104c3089a1f",
+"main.dart.js": "f55dffeace7cfb39ecb8eadb3df5561c",
 "manifest.json": "3a3100c1d8b5667e9492efa607a10a41",
 "version.json": "a894774ce4fb826c8dc3ce5ce2027b5f"};
 // The application shell files that are downloaded before a service worker can
